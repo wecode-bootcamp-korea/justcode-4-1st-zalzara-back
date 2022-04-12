accf50362b7a7d991330a70335a70b4312af0b94
HANSOL JEONG <losnah24@gmail.com>
2022-04-12 17:41:01 +0900

    Refactor: remove console.log

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,10 +3,8 @@ const cartService = require('../services/cartService')
 // webucks 의 getProduct 를 가져온 상태
 const addCart = async (req, res) => {
 	try {
-        console.log("controller line 1!!");
-        const { product_id } = req.body
-		const cart = await cartService.addCart(product_id)
-        console.log("controller!!");
+    const { product_id } = req.body
+    const cart = await cartService.addCart(product_id)
 
 		res.status(200).json({ message: cart })
 	} catch (err) {
